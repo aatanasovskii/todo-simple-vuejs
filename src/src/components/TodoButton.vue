1359fffd27f4d47b68e3e963c737ada9f397a716
Andrej Atanasovski <90504638+aatanasovskii@users.noreply.github.com>
2023-02-28 10:50:36 +0100

    Add files via upload

---
+++ src/src/components/TodoButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="button">
+    <form>
+      <div>
+        <label>Title: </label>
+        <input type="text" id="title" v-model="todo.title" required />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea id="description" v-model="todo.description" required>
+        </textarea>
+      </div>
+      <div>
+        <label>Category: </label>
+        <select id="category" v-model="todo.category" required>
+          <option value="Work">Work</option>
+          <option value="Personal">Personal</option>
+          <option value="Chores">Chores</option>
+        </select>
+      </div>
+      <div>
+        <label>Priority: </label>
+        <select id="priority" v-model="todo.priority" required>
+          <option value="Urgent">Urgent</option>
+          <option value="Normal">Normal</option>
+          <option value="Low">Low</option>
+        </select>
+      </div>
+      <button
+        type="submit"
+        @click="
+          addTodo();
+          showList();
+          visibleForm();
+        "
+      >
+        Add Todo Item
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: {},
+    };
+  },
+  methods: {
+    addTodo() {
+      this.$emit("add-todo", this.todo);
+      this.todo = {};
+      this.formVisible = false;
+    },
+    showList() {
+      this.$emit("show-list");
+    },
+    visibleForm() {
+      this.$emit("form-visible");
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list">
+    <ol type="1">
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo.title }} - {{ todo.category }},
+        {{ todo.priority }}
+        <button @click="editTodo(index)">Edit</button>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: Array,
+    selectedTodo: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    editTodo(index) {
+      this.$emit("edit-todo", index);
+    },
+  },
+};
+</script>
+
+<style></style>
